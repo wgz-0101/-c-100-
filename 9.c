cc2f7bf9f24cacc981b3af2140c4d26fe83a6a13
WGZ-0101 <2016066355@qq.com>
2026-09-09 11:11:49 +0800

    include<>和inlcude""的区别"

---
+++ 9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//include <>和include ""的区别
+//<>是系统头文件，""是用户自定义头文件
+//<>是编译器在系统目录下查找头文件，""是编译器在当前目录下查找头文件
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
